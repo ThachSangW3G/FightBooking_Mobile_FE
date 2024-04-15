a2ddcc7677f29007928a49e4e00b8ff09d380593
ThachSangW3G <thachsang2202@gmail.com>
2024-04-15 12:27:40 +0700

    update

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color green = Color(0xff28b446);
+  static const Color white = Color(0xffffffff);
+  static const Color black = Color(0x00000000);
+  static const Color gray = Color(0xffd9d9d9);
+  static const Color slamon = Color(0xffff8682);
+  static const Color stack = Color(0xff889088);
+  static const Color shamrock = Color(0xff2DCC70);
+  static const Color concrete = Color(0xfff2f2f2);
+  static const Color whisper = Color(0xffF4F5F9);
+}
---
+++ lib/constants/app_styles.dart
@@ -0,0 +1,7 @@
+import 'package:flightbooking_mobile_fe/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final kLableTextStyleBigGreen = GoogleFonts.inter(
+    textStyle: const TextStyle(
+        color: AppColors.green, fontWeight: FontWeight.w700, fontSize: 30.0));

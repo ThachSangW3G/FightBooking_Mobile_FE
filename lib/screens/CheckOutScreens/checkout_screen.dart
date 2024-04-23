06be4bb05cbe39d488ab62672bcd654e01232bc0
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-04-23 13:01:58 +0700

    Cập nhật thông tin hành khách

--- lib/screens/CheckOutScreens/checkout_screen.dart
+++ lib/screens/CheckOutScreens/checkout_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flightbooking_mobile_fe/constants/app_colors.dart';
 import 'package:flightbooking_mobile_fe/widgets/checkout/flight_info_widget.dart';
+import 'package:flightbooking_mobile_fe/widgets/checkout/flight_price_widget.dart';
 import 'package:flutter/material.dart';
 
 class CheckoutScreen extends StatefulWidget {
@@ -52,7 +53,20 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
     }
     // Add more flight data here if needed
   ];
-  bool isExpanded = true;
+  final List<Map<String, dynamic>> flightPrice = [
+    {
+      'adults': 2,
+      'children': 1,
+      'infants': 1,
+      'priceAdult': 100,
+      'priceChild': 80,
+      'priceInfant': 40,
+      'totalPrice': 220
+    }
+  ];
+  bool isExpandedDetails = true;
+  bool isExpandedPriceDetails = true;
+  bool isExpandedPassenger = true;
 
   @override
   Widget build(BuildContext context) {
@@ -76,40 +90,27 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
         ),
       ),
       body: Container(
-        padding: const EdgeInsets.all(16.0),
-        decoration: BoxDecoration(
-          color: AppColors.white,
-          border: Border.all(width: 0.1),
-          borderRadius: BorderRadius.all(Radius.circular(8.0)),
-        ),
+        padding: const EdgeInsets.symmetric(horizontal: 16),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const SizedBox(height: 0), // Add space above "Chi tiết đơn hàng"
               _buildFlightList(), // Danh sách chuyến bay
+              Divider(),
               const SizedBox(height: 0),
-              const Text(
-                'Chi tiết giá',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+              _buildPriceList(),
+              Divider(),
               // Thêm danh sách các mục chi tiết giá ở đây
               const SizedBox(height: 0),
-              const Text(
-                'Thông tin khách hàng',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+              _buildPassengerList(),
+              Divider(),
               // Thêm danh sách thông tin khách hàng ở đây
             ],
           ),
         ),
       ),
+      bottomNavigationBar: _buildTotalSection(),
     );
   }
 
@@ -119,7 +120,7 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
       children: [
         const SizedBox(height: 0), // Add space above "Chi tiết đơn hàng"
         Padding(
-          padding: const EdgeInsets.only(bottom: 8.0), // Add padding bottom
+          padding: const EdgeInsets.only(bottom: 2.0), // Add padding bottom
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -133,19 +134,17 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
               IconButton(
                 onPressed: () {
                   setState(() {
-                    isExpanded = !isExpanded;
+                    isExpandedDetails = !isExpandedDetails;
                   });
                 },
-                icon: Icon(
-                  isExpanded
-                      ? Icons.keyboard_arrow_up
-                      : Icons.keyboard_arrow_down,
-                ),
+                icon: Icon(isExpandedDetails
+                    ? Icons.keyboard_arrow_down
+                    : Icons.keyboard_arrow_right),
               ),
             ],
           ),
         ),
-        if (isExpanded)
+        if (isExpandedDetails)
           ListView.builder(
             shrinkWrap: true,
             physics: NeverScrollableScrollPhysics(),
@@ -174,4 +173,169 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
       ],
     );
   }
+
+  Widget _buildPriceList() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 0), // Add space above "Chi tiết đơn hàng"
+        Padding(
+          padding: const EdgeInsets.only(bottom: 2.0), // Add padding bottom
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Chi tiết giá',
+                style: TextStyle(
+                  fontSize: 18, // Increase font size for "Chi tiết đơn hàng"
+                  fontWeight: FontWeight.bold, // Make it bold
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    isExpandedPriceDetails = !isExpandedPriceDetails;
+                  });
+                },
+                icon: Icon(isExpandedPriceDetails
+                    ? Icons.keyboard_arrow_down
+                    : Icons.keyboard_arrow_right),
+              ),
+            ],
+          ),
+        ),
+        if (isExpandedPriceDetails)
+          ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: flightPrice.length,
+            itemBuilder: (context, index) {
+              final flightPrices = flightPrice[index];
+              return Padding(
+                padding:
+                    const EdgeInsets.only(bottom: 15.0), // Add padding bottom
+                child: FlightPriceDetailsWidget(
+                  // Truyền dữ liệu chuyến bay vào FlightInfoWidget
+                  adults: flightPrices['adults'],
+                  children: flightPrices['children'],
+                  infants: flightPrices['infants'],
+                  priceAdult: flightPrices['priceAdult'],
+                  priceChild: flightPrices['priceChild'],
+                  priceInfant: flightPrices['priceInfant'],
+                  totalPrice: flightPrices['totalPrice'],
+                ),
+              );
+            },
+          ),
+      ],
+    );
+  }
+
+  Widget _buildPassengerList() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 0), // Add space above "Chi tiết đơn hàng"
+        Padding(
+          padding: const EdgeInsets.only(bottom: 2.0), // Add padding bottom
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Thông tin hành khách',
+                style: TextStyle(
+                  fontSize: 18, // Increase font size for "Chi tiết đơn hàng"
+                  fontWeight: FontWeight.bold, // Make it bold
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    isExpandedPassenger = !isExpandedPassenger;
+                  });
+                },
+                icon: Icon(isExpandedPassenger
+                    ? Icons.keyboard_arrow_down
+                    : Icons.keyboard_arrow_right),
+              ),
+            ],
+          ),
+        ),
+        if (isExpandedPassenger)
+          ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: flightPrice.length,
+            itemBuilder: (context, index) {
+              final flightPrices = flightPrice[index];
+              return Padding(
+                padding:
+                    const EdgeInsets.only(bottom: 15.0), // Add padding bottom
+                child: FlightPriceDetailsWidget(
+                  // Truyền dữ liệu chuyến bay vào FlightInfoWidget
+                  adults: flightPrices['adults'],
+                  children: flightPrices['children'],
+                  infants: flightPrices['infants'],
+                  priceAdult: flightPrices['priceAdult'],
+                  priceChild: flightPrices['priceChild'],
+                  priceInfant: flightPrices['priceInfant'],
+                  totalPrice: flightPrices['totalPrice'],
+                ),
+              );
+            },
+          ),
+      ],
+    );
+  }
+}
+
+Widget _buildTotalSection() {
+  return Container(
+    color: Colors.white, // Màu nền cho container
+    padding: EdgeInsets.only(left: 16, right: 16, bottom: 20, top: 10),
+    child: SafeArea(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Tổng cộng:',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                '12.400.000 đ', // Cập nhật giá tiền đúng
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue),
+              ),
+            ],
+          ),
+          SizedBox(height: 15), // Khoảng cách giữa text và button
+          ElevatedButton(
+            onPressed: () {
+              // Xử lý sự kiện khi nhấn nút thanh toán
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.dodger, // Màu nền của nút
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10), // Bo tròn góc của nút
+              ),
+              padding: EdgeInsets.symmetric(
+                  vertical: 10), // Đệm dọc lớn hơn để nút cao hơn
+              minimumSize: Size(double.infinity,
+                  56), // Chiều dài tối thiểu của nút (double.infinity sẽ làm nút dài ra cùng với chiều rộng của parent widget)
+              textStyle: TextStyle(fontSize: 18), // Kích thước chữ của nút
+            ),
+            child: Text(
+              'Thanh toán',
+              style: TextStyle(color: Colors.white), // Màu chữ của nút
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 }
--- lib/widgets/checkout/flight_price_widget.dart
+++ lib/widgets/checkout/flight_price_widget.dart
@@ -2,28 +2,22 @@ import 'package:flutter/material.dart';
 
 class FlightPriceDetailsWidget extends StatelessWidget {
   final int adults;
-  final double priceAdult;
-  final int infants;
-  final double priceInfant;
   final int children;
-  final double priceChild;
-  final String tax;
-  final String baggage;
-  final double priceBaggage;
-  final double priceInsurance;
+  final int infants;
+  final int priceAdult;
+  final int priceChild;
+  final int priceInfant;
+  final int totalPrice;
 
   const FlightPriceDetailsWidget({
     Key? key,
     required this.adults,
-    required this.priceAdult,
-    required this.infants,
-    required this.priceInfant,
     required this.children,
+    required this.infants,
+    required this.priceAdult,
     required this.priceChild,
-    required this.tax,
-    required this.baggage,
-    required this.priceBaggage,
-    required this.priceInsurance,
+    required this.priceInfant,
+    required this.totalPrice,
   }) : super(key: key);
 
   @override
@@ -31,21 +25,61 @@ class FlightPriceDetailsWidget extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          'Chi tiết giá',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
+        if (adults > 0)
+          _buildPriceRow("người lớn", adults, priceAdult * adults),
+        if (children > 0)
+          _buildPriceRow("trẻ em", children, priceChild * children),
+        if (infants > 0)
+          _buildPriceRow("em bé", infants, priceInfant * infants),
+        _buildTotalRow("Tổng cộng", totalPrice),
+      ],
+    );
+  }
+
+  Widget _buildPriceRow(String title, int count, int price) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "$count $title",
+              style: TextStyle(
+                fontSize: 14,
+              ),
+            ),
+            Text(
+              "\$$price",
+              style: TextStyle(
+                fontSize: 14,
+              ),
+            ),
+          ],
         ),
-        SizedBox(height: 8),
-        Text('Adults: $adults - Price: $priceAdult'),
-        Text('Infants: $infants - Price: $priceInfant'),
-        Text('Children: $children - Price: $priceChild'),
-        Text('Tax: $tax'),
-        Text('Baggage: $baggage - Price Baggage: $priceBaggage'),
-        Text('Price Insurance: $priceInsurance'),
-        // Các thông tin khác có thể thêm vào ở đây
+        SizedBox(height: 5),
+        Divider(),
+      ],
+    );
+  }
+
+  Widget _buildTotalRow(String title, int totalPrice) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "$title",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "\$$totalPrice",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ],
     );
   }
